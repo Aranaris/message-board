489502422ca8c626d05a2363aefc0548f065eef6
vinceye <vinceye.cm@gmail.com>
2023-09-27 17:21:55 -0700

    adding initial user schema

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {type: String, required: true, maxLength: 50},
+    first_name: {type: String, maxLength: 50},
+    date_of_birth: {type: Date},
+    last_activity: {type: Date, default: new Date()},
+});
+
+userSchema.virtual('name').get(function() {
+    let name = "";
+    if (this.first_name) {
+        name = this.first_name;
+    }
+
+    return name;
+});
+
+userSchema.virtual('url').get(function() {
+    return `/users/profile/${this._id}`;
+});
+
+module.exports = mongoose.model('User', userSchema);
